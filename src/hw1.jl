49ab248c6b4c92236db997f6867ef71989715643
dhananjay <djghei@gmail.com>
2018-10-30 00:04:44 -0500

    Adding code files

---
+++ src/hw1.jl
@@ -0,0 +1,19 @@
+#-----------------------------
+#       Search Models
+# Author: Dhananjay Ghei
+# Date: October 29, 2018
+#-----------------------------
+
+# Loading the required libraries
+using ForwardDiff
+using NLSolversBase
+using NLsolve
+using SymPy
+using LinearAlgebra
+
+#------------------------------ Setting up the parameters
+β, s̄, τ, x, ϕ, γ, α, δ, ρ, ζ = .996, .0012, .4, .034, .5, .471, .33, .0028, .4, .00325
+
+# Euler equation
+
+# Intertemporal equation
